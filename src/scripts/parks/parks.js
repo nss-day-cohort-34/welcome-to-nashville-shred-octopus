e277da199d5c0b036e7a4d43e2ead40b97cd39b0
Haroon Iqbal <harooniqbal@Haroons-MacBook-Pro.local>
2019-07-26 15:49:19 -0500

    added save button

--- src/scripts/parks/parks.js
+++ src/scripts/parks/parks.js
@@ -1,20 +1,20 @@
 //fetch data from parks api
 
-const fetcher = () => {
-    fetch("https://data.nashville.gov/resource/74d7-b74t.json?$$app_token=cjh7CiBKoXfwugBplSHY8PgiU")
-    .then(data => data.json())
-    .then(parks => {
-        console.log(parks)
-    }
-)}
+// const fetcher = () => {
+//     fetch("https://data.nashville.gov/resource/74d7-b74t.json?$$app_token=cjh7CiBKoXfwugBplSHY8PgiU")
+//     .then(data => data.json())
+//     .then(parks => {
+//         console.log(parks)
+//     }
+// )}
 
-fetcher();
+// fetcher();
 
-//create function to capture user input in variable
-const userInput = document.getElementById("park__searchbox")
-const getInput = () => {
-    return userInput.value
-}
+// //create function to capture user input in variable
+// const userInput = document.getElementById("park__searchbox")
+// const getInput = () => {
+//     return userInput.value
+// }
 
 
 //create event listener to trigger search upon button click
@@ -45,8 +45,13 @@ const parkAsHTML = (object) => {
     return `
     <section class="parkResultList">
         <h3>${object.park_name}
-        <h3>${object.mapped_location.human_address.replace('{"address": "', "").replace('", "city": "', ", ").replace('", "state": "', ", ").replace('", "zip": ""}', "")}
-    </section>
+        <h3>${object.mapped_location.human_address.replace('{"address": "', "").replace('", "city": "', ", ").replace('", "state": "', ", ").replace('", "zip": ""}', "")
+        .replace('", "zip": "37206"}', "")
+        .replace('", "zip": "37080"}', "").replace('", "zip": "37013"}', "").replace('", "zip": "37138"}', "").replace('", "zip": "37211"}', "")
+        .replace('", "zip": "37212"}', "").replace('", "zip": "37205"}', "").replace('", "zip": "37027"}', "").replace('", "zip": "37214"}', "")
+        .replace('", "zip": "37209"}', "").replace('", "zip": "37076"}', "").replace('", "zip": "37115"}', "")}
+        </section>
+    <button id="park__savebutton">Save</button>
     `
 }
 
@@ -56,3 +61,5 @@ const container = document.querySelector(".results__List")
 const parkFactory = (HTMLRepresentation) => {
     container.innerHTML += HTMLRepresentation;
 }
+
+//create event listener to save result to the itinerary
